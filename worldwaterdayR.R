5639a79598f79c92300da9e52e54d6eda8f76ee5
Mike Daems <m.a.l.daems@tilburguniversity.edu>
2021-04-16 13:51:42 +0200

    rtweet package installed

--- worldwaterdayR.R
+++ worldwaterdayR.R
@@ -1,11 +1,12 @@
 #demo 1: visualize tweet network
-
+install.packages("rtweet")
 library(rtweet)
+
 
 tweets <- search_tweets(
   "#worldwaterday",
   n = 500,
   include_rts = TRUE
-  
 )
-li
+  
+
